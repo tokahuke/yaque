ffe1cf22a9834e50d44bf8081f22d2a5db1c018a
Russell Mull <russell@auxon.io>
2023-10-19 15:34:02 -0700

    Add Recevier::recv_batch_up_to, try_recv_batch_up_to
    This adds a new batch recv variant, aimed at applications where
    message processing latency is important but batch processing can still
    be valuable.

--- fuzz/fuzz_targets/queue_operations.rs
+++ fuzz/fuzz_targets/queue_operations.rs
@@ -1,13 +1,13 @@
 #![no_main]
 
-use libfuzzer_sys::arbitrary::Arbitrary;
+use libfuzzer_sys::arbitrary::{Arbitrary, Unstructured};
 use libfuzzer_sys::fuzz_target;
+use log::trace;
+use std::collections::VecDeque;
 use tempfile::TempDir;
 use yaque::{channel, TryRecvError};
-use std::collections::VecDeque;
 
-
-fuzz_target!(|scenario: Scenario| { scenario.execute() });
+fuzz_target!(|scenario: Scenario| scenario.execute());
 
 #[derive(Arbitrary, Debug)]
 struct Scenario {
@@ -16,6 +16,8 @@ struct Scenario {
 
 impl Scenario {
     pub fn execute(&self) {
+        //simple_logger::SimpleLogger::new().env().init().unwrap();
+
         let base_dir = std::env::var("YAQUE_FUZZ_BASE_DIR").expect(
             "You must set the YAQUE_FUZZ_BASE_DIR environment variable, preferrably to a ramdisk",
         );
@@ -24,30 +26,60 @@ impl Scenario {
         let (mut tx, mut rx) = channel(&queue_dir).unwrap();
         let mut pending_sent_items: VecDeque<Vec<u8>> = Default::default();
 
-        for command in self.commands.iter() {
+        for (n, command) in self.commands.iter().enumerate() {
+            trace!("Command {}: {command:?}", n + 1);
+
             match command {
-                QueueCommand::Send(buffer) => {
-                    tx.try_send(buffer).expect("Send");
-                    pending_sent_items.push_back(buffer.clone());
+                QueueCommand::Send(msg) => {
+                    tx.try_send(msg.0.clone()).expect("Send");
+                    pending_sent_items.push_back(msg.0.clone());
                 }
-                QueueCommand::SendBatch(buffers) =>  {
-                    tx.try_send_batch(buffers).expect("SendBatch");
-                    pending_sent_items.extend(buffers.clone());
+                QueueCommand::SendBatch(msgs) => {
+                    let buffers = msgs.iter().map(|m| m.0.clone()).collect::<Vec<_>>();
+                    tx.try_send_batch(buffers.clone()).expect("SendBatch");
+                    pending_sent_items.extend(buffers);
                 }
-                QueueCommand::Recv =>  {
-                    match rx.try_recv() {
-                        Ok(item) => {
-                            let item = item.try_into_inner().unwrap();
-                            let pending = pending_sent_items.pop_front().unwrap();
-                            assert_eq!(pending, item);
-                        }
-                        Err(TryRecvError::Io(io)) => panic!("{io:?}"),
-                        Err(TryRecvError::QueueEmpty) => ()
+                QueueCommand::Recv => match rx.try_recv() {
+                    Ok(item) => {
+                        let item = item.try_into_inner().unwrap();
+                        let pending = pending_sent_items.pop_front().unwrap();
+                        assert_eq!(pending, item);
                     }
-                }
-                QueueCommand::RecvBatch(count) =>  {
-                    match rx.try_recv_batch(*count) {
-                        Ok(items) =>  {
+                    Err(TryRecvError::Io(io)) => panic!("{io:?}"),
+                    Err(TryRecvError::QueueEmpty) => {
+                        assert!(pending_sent_items.is_empty());
+                    }
+                },
+
+                // QueueCommand::RecvBatch(count) => match rx.try_recv_batch(*count as usize) {
+                //     Ok(items) => {
+                //         assert!(
+                //             *count as usize <= pending_sent_items.len(),
+                //             "Should get data when batch is is <= available count"
+                //         );
+                //         let items = items.try_into_inner().unwrap();
+                //         for item in items.into_iter() {
+                //             let pending = pending_sent_items.pop_front().unwrap();
+                //             assert_eq!(pending, item);
+                //         }
+                //     }
+                //     Err(TryRecvError::Io(io)) => panic!("{io:?}"),
+                //     Err(TryRecvError::QueueEmpty) => {
+                //         trace!(
+                //             "got QueueEmpty. count: {}, pending_sent_items.len(): {}",
+                //             &count,
+                //             pending_sent_items.len()
+                //         );
+                //         assert!(
+                //             *count as usize > pending_sent_items.len(),
+                //             "Should see QueueEmpty when batch size > available count"
+                //         )
+                //     }
+                // },
+
+                QueueCommand::RecvBatchUpTo(count) => {
+                    match rx.try_recv_batch_up_to(*count as usize) {
+                        Ok(items) => {
                             let items = items.try_into_inner().unwrap();
                             for item in items.into_iter() {
                                 let pending = pending_sent_items.pop_front().unwrap();
@@ -55,7 +87,9 @@ impl Scenario {
                             }
                         }
                         Err(TryRecvError::Io(io)) => panic!("{io:?}"),
-                        Err(TryRecvError::QueueEmpty) => ()
+                        Err(TryRecvError::QueueEmpty) => {
+                            assert!(pending_sent_items.is_empty());
+                        }
                     }
                 }
             }
@@ -65,8 +99,20 @@ impl Scenario {
 
 #[derive(Arbitrary, Debug)]
 enum QueueCommand {
-    Send(Vec<u8>),
-    SendBatch(Vec<Vec<u8>>),
+    Send(NonEmptyMsg),
+    SendBatch(Vec<NonEmptyMsg>),
     Recv,
-    RecvBatch(usize),
+    //RecvBatch(u16),
+    RecvBatchUpTo(u16),
+}
+
+#[derive(Debug)]
+struct NonEmptyMsg(Vec<u8>);
+
+impl<'a> Arbitrary<'a> for NonEmptyMsg {
+    fn arbitrary(u: &mut Unstructured<'a>) -> Result<Self, arbitrary::Error> {
+        let mut msg = vec![u8::arbitrary(u)?];
+        msg.extend(Vec::<u8>::arbitrary(u)?);
+        Ok(NonEmptyMsg(msg))
+    }
 }
